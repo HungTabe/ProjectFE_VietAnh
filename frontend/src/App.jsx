174e584a516c5e1ccfdfb634b805973f8b6743e5
tuantu04 <tuantu04@gmail.com>
2025-06-23 22:46:39 +0700

    [vian_6] Full page with MockData

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import {Routes, Route, Navigate} from "react-router-dom";
 import {Dashboard, Auth, User} from "@/layouts";
 import UserDetail from "@/pages/dashboard/userDetail.jsx";
+import CoursePage from "@/pages/user/course-page/course-page";
+import CourseDetailPage from "@/pages/user/course-page/course-detail-page/course-detail-page";
+
+
 
 function App() {
     return (
@@ -8,6 +12,8 @@ function App() {
             <Route path="/dashboard/*" element={<Dashboard/>}/>
             <Route path="/auth/*" element={<Auth/>}/>
             <Route path="/user/*" element={<User/>}/>
+            <Route path="/test" element={<CoursePage/>}/>
+            <Route path="/test2" element={<CourseDetailPage/>}/>
             <Route path="*" element={<Navigate to="/dashboard/home" replace/>}/>
         </Routes>
     );
